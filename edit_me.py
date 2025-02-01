6cf1cfed39da1112ae285cd86237e4d5eaa6fac5
KaydenF-529781 <kbfuchs06@gmail.com>
2025-01-31 20:14:32 -0600

    Kayden Fuchs - Lab 2 Complete

--- edit_me.py
+++ edit_me.py
@@ -1,11 +1,22 @@
-
 def monotonic(lst):
     """Return True if lst is monotonic; return False, otherwise."""
-    #
-    # YOUR IMPLEMENTATION GOES HERE
-    #
+    increasing = True
+    decreasing = True
 
-#
-# Feel free to replace these comments with
-# code that tests your function monotonic.
-#
+    for i in range(1,len(lst)):
+        
+
+        if lst[i] < lst[i-1]:
+            decreasing = False
+        if lst[i] > lst[i-1]:
+            increasing = False
+        
+    return increasing or decreasing
+
+list1 = [1,2,3,4,5]
+list2 = [10,9,8,7,6]
+list3 = [4,7,2,4,9]
+
+print(monotonic(list1))
+print(monotonic(list2))
+print(monotonic(list3))
